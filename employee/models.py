9f611882fb9bed7f4ea2df29c28829484df33c55
Cory Kitchens <corykitchens@gmail.com>
2016-01-11 16:04:35 -0800

    Landing page sent to login form

--- employee/models.py
+++ employee/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+	first_name = models.CharField(max_length=200)
+	middle_initial = models.CharField(max_length=1)
+	last_name = models.CharField(max_length=200)
+	job_title = models.ForeignKey('Job')
+	wage = models.FloatField()
+	birthdate = models.DateField()
+	sex		= models.BooleanField()
+
+	def __str__(self):
+		return self.first_name, self.last_name
+
+
+
+class Job(models.Model):
+	title = models.CharField(max_length=200)
--- sae/settings.py
+++ sae/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authenticate'
+    'employee'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'sae.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ('templates',),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
