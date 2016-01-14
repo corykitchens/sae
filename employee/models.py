fe030c763c6d59a1cd6f115697e70911fbbe7983
Cory Kitchens <corykitchens@gmail.com>
2016-01-13 20:17:13 -0800

    Removed job table and fix str method

--- employee/models.py
+++ employee/models.py
@@ -4,22 +4,25 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
+	JOB_TITLES = (
+		('mgmt', 'Management'),
+		('admin', 'Administrative'),
+		('tech', 'Service Technician')
+	)
+
 	ssn = models.IntegerField(primary_key=True)
 	first_name = models.CharField(max_length=200)
 	middle_initial = models.CharField(max_length=1)
 	last_name = models.CharField(max_length=200)
-	job_title = models.ForeignKey('Job')
+	job_title = models.CharField(max_length=200, choices=JOB_TITLES)
 	wage = models.FloatField()
 	birthdate = models.DateField()
-	sex		= models.BooleanField()
+	sex	= models.BooleanField()
+
+	
 
 	class Meta:
 		db_table = "CKTM_EMPLOYEE"
 
 	def __str__(self):
-		return self.first_name self.last_name
-
-
-
-class Job(models.Model):
-	title = models.CharField(max_length=200)
+		return self.first_name + " " + self.last_name
