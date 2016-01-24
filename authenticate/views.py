8f19e88df7eb01d82e2c1fcca03a4946ae78a13a
Cory Kitchens <corykitchens@gmail.com>
2016-01-24 09:01:57 -0800

    User model mapping to Employee

--- authenticate/views.py
+++ authenticate/views.py
@@ -2,8 +2,8 @@ import sys
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-from .forms import LoginUserForm
+from django.contrib.auth import authenticate, login as auth_login 
+from employee.models import Employee
 
 # Create your views here.
 def login(request):
@@ -15,9 +15,10 @@ def login(request):
 		if user is not None:
 			
 			if user.is_active:
-				
-				#login(request, user)
-				return render(request, 'employee/profile_home.html', {'user' : user})
+				auth_login(request, user)
+				employee = Employee.objects.get(user=user)
+
+				return render(request, 'employee/profile_home.html', {'user' : user, 'employee' : employee})
 			else:
 				return HttpResponse("Could not login")
 		else:
--- employee/models.py
+++ employee/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Employee(models.Model):
@@ -22,7 +23,12 @@ class Employee(models.Model):
 	wage = models.FloatField()
 	birthdate = models.DateField(null=True)
 	sex	= models.CharField(max_length=10, choices=SEX_CHOICES)
-
+	user = models.OneToOneField(
+		User,
+		on_delete=models.CASCADE,
+		related_name="useraccount",
+		null=True
+		)
 	
 
 	class Meta:
