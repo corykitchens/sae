9d6a810879433893a6c4b5cb18dd00885c48c3b3
Cory Kitchens <corykitchens@gmail.com>
2016-01-24 08:45:00 -0800

    Basic user auth

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,5 +1,33 @@
+import sys
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from .forms import LoginUserForm
+
 # Create your views here.
-def index(request):
-	return render(request, 'login/login.html')
+def login(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		print >>sys.stderr, user
+		if user is not None:
+			
+			if user.is_active:
+				
+				#login(request, user)
+				return render(request, 'employee/profile_home.html', {'user' : user})
+			else:
+				return HttpResponse("Could not login")
+		else:
+			return HttpResponse('Could not authenticate')
+		# form = LoginUserForm(request.POST)
+
+		# if form.is_valid():
+		# 	return HttpResponse('Worked')
+		# else:
+		# 	return HttpResponse('Failed validity')
+	else:
+		#form = LoginUserForm()
+		return render(request, 'login/login.html', {})
