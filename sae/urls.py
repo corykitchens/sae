f1671019d2b0a217d8c99b0d1ad4482b392bf9ba
Cory Kitchens <corykitchens@gmail.com>
2016-01-30 13:02:03 -0800

    Fixed Employee redirect

--- sae/urls.py
+++ sae/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^', include('authenticate.urls')),
-    #Employee module
     url(r'^employees/', include('employee.urls')),
     url(r'^customers/', include('customer.urls')),
     url(r'^admin/', admin.site.urls),
