f0b61fa25ef78da44031c2c1cdf6636628e9368e
Cory Kitchens <corykitchens@gmail.com>
2016-01-31 13:53:11 -0800

    Updates from Ty

--- customer/urls.py
+++ customer/urls.py
@@ -5,10 +5,4 @@ urlpatterns = [
 	#landing page
 	url(r'^customer_directory', CustomerDirectory.as_view(), name="customer_index"),
 	url(r'^edit_profile/(?P<customer_id>[0-9])$', customer_profile, name="customer_profile")
-	# r'^ <- begin of raw string
-	# $' <- end of raw string
-	#url(r'^(?P<test>[a-z]{2})$', views.a)
-
-
-
 ]
