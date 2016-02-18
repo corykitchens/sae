8dac2982fce767cd4226c1a0dbcb553c362fed31
TyAnthoney Morrell <tyanthoney14@gmail.com>
2016-02-18 08:45:55 -0800

    Adding Cory's recent changes.

--- workorder/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import WorkOrder
-
-class WorkOrderForm(ModelForm):
-	class Meta:
-		model = WorkOrder
-		fields = ['date_created','vehicle','service_type']
--- workorder/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from .views import create_work_order
-
-urlpatterns = [
-	url(r'^', create_work_order, name="create_work_order")
-]
