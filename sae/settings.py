5472bcebb26606bb97e4669e639b048b963df123
Cory Kitchens <corykitchens@gmail.com>
2016-03-09 14:13:53 -0800

    Debug

--- sae/settings.py
+++ sae/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '0guxvpy(ol_kush)#^osd^^pph^04s^wyncb3=!@pfe&)^0e6%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
@@ -133,7 +133,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
